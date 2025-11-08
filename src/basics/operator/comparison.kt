6afb07d3f6ab8e85f403acae4bf764e17c1d2cd9
Kaizen <gibrantsyareza@gmail.com>
2025-11-08 22:02:29 +0700

    belajar operator perbandingan

---
+++ src/basics/operator/comparison.kt
@@ -0,0 +1,22 @@
+package basics.operator
+
+fun main(){
+    // digunakan untuk pengkondisian boolean
+    val a = 5
+    val b = 5
+    val isEqual = (a == b) // true
+
+    val x = 10
+    val y = 7
+    val notEqual = (x != y) // true
+
+    val num1 = 100
+    val num2 = 95
+    val greaterThan = num2 > num1 // false
+    val rightGreaterThan = num1 > num2 // true
+
+    println(isEqual)
+    println(notEqual)
+    println(greaterThan)
+    println(rightGreaterThan)
+}
