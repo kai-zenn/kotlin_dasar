a549bfc86160f8294302bf71c91fbc7ff2f17ee8
Kaizen <gibrantsyareza@gmail.com>
2025-11-08 19:49:50 +0700

    belajar syntax variable di kotlin

--- src/basics/variable.kt
+++ src/basics/variable.kt
@@ -1,8 +1,17 @@
 package basics
 
-val name = "jokowi" // immutable atau tidak berubah
-var age = 666 // mutable atau bisa berubah nilainya
-
 fun main(){
+    // dynamically typing in kotlin
+    val name = "jokowi" // immutable atau tidak berubah
+    var age = 666 // mutable atau bisa berubah nilainya
+
+    // typesafe in kotlin
+    var namaPertama: String = "kotlin" // sah aja, tapi redundant
+    var namaKedua: String = "keren" // kotlin sudah memiliki fitur dynamic typing
+    var umurGwe: Int = 17
+    // mendeklarasikan value setelah memberikan tipe kepada variabel
+    var namaGwe: String
+    namaGwe = "gibrant"
+
     println("halo nama saya $name, saya berumur $age")
 }
