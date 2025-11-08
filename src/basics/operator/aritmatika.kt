bf66bfce35a205e9c794262d6a20b8909c7f7c9f
Kaizen <gibrantsyareza@gmail.com>
2025-11-08 21:38:48 +0700

    belajar operator aritmatika

---
+++ src/basics/operator/aritmatika.kt
@@ -0,0 +1,32 @@
+package basics.operator
+
+fun main() {
+    var nomor: Int = 100
+
+    // addition (+)
+    var tambah = nomor + 10
+
+    // subtraction (-)
+    var kurang = nomor - 10
+
+    // multiplication (*)
+    var perkalian = nomor * 5
+
+    // division (/)
+    var pembagian = nomor / 10
+
+    // modulus (%), sisa pembagian
+    var modulus = nomor % 9
+
+    // increment (++), angka akan bertambah terus sesuai dengan kondisi yang diberikan nanti
+    // var increment = ++nomor
+
+    // decrement (--), kebalikan dari increment
+    // var decrement = --nomor
+
+    println(tambah)
+    println(kurang)
+    println(perkalian)
+    println(pembagian)
+    println(modulus)
+}
---
+++ src/basics/operator/basic.kt
@@ -0,0 +1,8 @@
+package basics.operator
+
+fun main(){
+    var x = 100 + 50
+    // 100 dan 50 sebagai operand
+    // + adalah operator
+    print(x)
+}
