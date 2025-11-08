5d578ba24724dba367e4b58f8d89e62964e15017
Kaizen <gibrantsyareza@gmail.com>
2025-11-08 21:51:18 +0700

    belajar operator penugasan

---
+++ src/basics/operator/assignment.kt
@@ -0,0 +1,15 @@
+package basics.operator
+
+fun main(){
+
+    // assignment/penugasan operator digunakan untuk menetapkan nilai ke variabel
+    var x = 100
+
+    // x += 10 hasilnya 110
+    // x -= 5 hasilnya 95
+    // x *= 2 hasilnya 200
+    // x /= 10 hasilnya 10
+    // x %= 9 hasilnya 1
+
+    print(x)
+}
