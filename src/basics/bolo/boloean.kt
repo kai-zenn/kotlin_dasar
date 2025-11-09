642e7c9df855554bf77e644201cdd8c9ded02274
Kaizen <gibrantsyareza@gmail.com>
2025-11-09 12:55:09 +0700

    belajar boolean basik

---
+++ src/basics/bolo/boloean.kt
@@ -0,0 +1,15 @@
+package basics.bolo
+
+fun main(){
+    // boolean expression
+    val x = 10
+    val y = 9
+    println(x > y) // menghasilkan nilai true
+
+    // boolean values
+    // tipe boolean bisa deklarasikan dengan Boolean lalu harus diikuti dengan value TRUE atau FALSE
+    val kotlinGacor: Boolean = true
+    val nodeJsJelek: Boolean = false
+    println(kotlinGacor)
+    println(nodeJsJelek)
+}
