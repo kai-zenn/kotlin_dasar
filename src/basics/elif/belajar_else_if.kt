727f45945d7167c9ea8e89e92a9cdcd660a74839
Kaizen <gibrantsyareza@gmail.com>
2025-11-10 22:43:09 +0700

    belajar if else if else statement

---
+++ src/basics/elif/belajar_else_if.kt
@@ -0,0 +1,21 @@
+package basics.elif
+
+fun main(){
+    val x = 100;
+    val y = 75;
+
+    if (x >= y) {
+        println("x ternyata lebih gede jir")
+    } else {
+        println("y yang lebih gede ternyata")
+    }
+
+    val jam = 22
+    if (jam < 10) {
+        println("pagi gusy")
+    } else if (jam < 20) {
+        println("gud dey")
+    } else {
+        println("malam gusy")
+    }
+}
