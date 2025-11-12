35d06f3d6d77436d1df34e421ee4993a2ef2e293
Kaizen <gibrantsyareza@gmail.com>
2025-11-12 12:47:31 +0700

    belajar collection di kotlin

--- src/basics/collection/dataArray.kt
+++ src/basics/collection/dataArray.kt
@@ -7,9 +7,12 @@ fun main(){
     // dynamic array
     val sekolah = arrayOf("smk40", "smk46", "smk26", "smk48")
 
+    // mengubah nilai dalam array, karena array bersifat mutable
+    sekolah[0] = "smk1"
+
     println("saya kelas ${kelas[1]}")
     println("saya dari jurusan ${jurusan[0]}")
-    println("saya sekolah di ${sekolah[2]}")
+    println("saya sekolah di ${sekolah[0]}")
 
     // in operator untuk mengecek apakah ada
     if ("PPLG" in jurusan) {
--- src/basics/collection/dataList.kt
+++ src/basics/collection/dataList.kt
@@ -1,2 +1,13 @@
 package basics.collection
 
+fun main(){
+    //immutable list
+    val etalase: List<String> = listOf("indomie", "mie gaga", "mie sedap");
+    //mutable list
+    val mutableList = mutableListOf("shaka", "sakha", "sahka", "saput");
+    mutableList.add("burung gede") // Add an element
+    mutableList.removeAt(0) // Remove an element by index
+
+    println(etalase)
+    println(mutableList)
+}
