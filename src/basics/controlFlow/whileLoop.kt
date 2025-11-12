16bf97b0dff392c21964271197e46f58ae9aa56f
Kaizen <gibrantsyareza@gmail.com>
2025-11-12 22:01:04 +0700

    melanjutkan belajar while loop

--- src/basics/controlFlow/whileLoop.kt
+++ src/basics/controlFlow/whileLoop.kt
@@ -4,20 +4,40 @@ fun main(){
     var i = 0;
     var condition = false;
 
-    // selama kondisi terpenuhi, akan melakukan iterasi
-    while (i <= 10) {
-        // berisi aksi / iterasi
-        println(i)
-        ++i
-    }
+    /* selama kondisi belum terpenuhi, akan melakukan iterasi */
+//    while (i <= 10) {
+//        // berisi aksi / iterasi
+//        println(i)
+//        ++i
+//    }
 
     while (condition) {
         println("hasil while condition")
     } // tidak akan menghasilkan iterasi karena kondisinya tidak terpenuhi
 
-    // mengekesekusi kode didalamnya minimal sekali
+
+    /* mengekesekusi kode didalamnya minimal sekali */
     do {
         // tidak termasuk kedalam loop meskipun sejak awal kondisinya tidak terpenuhi
         println("hasil print iterasi")
     } while (condition)
+
+   // break, untuk keluar dari loop
+    while (i < 10) {
+        println("hasil dari loop dengan break: $i")
+        ++i
+        if (i == 8)
+            break
+    }
+
+    // continue, break satu iterasi lalu melanjutkan dengan iterasi berikutnya jika kondisi sudah terpenuhi
+    while (i <= 10) {
+        if (i <= 4) {
+            i++
+            continue
+        }
+        println("hasil dari loop dengan continue: $i")
+        i++
+    } // outputnya akan dimulai dari 5, karena meng skip 4
+
 }
