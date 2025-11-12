f92484120000afbd316d76473e0223a67f7460c6
Kaizen <gibrantsyareza@gmail.com>
2025-11-12 10:29:32 +0700

    belajar tipe data array

---
+++ src/basics/collection/dataArray.kt
@@ -0,0 +1,23 @@
+package basics.collection
+
+fun main(){
+    // type static array
+    val kelas:Array<Int> = arrayOf(10, 11, 12, 13);
+    val jurusan:Array<String> = arrayOf("PPLG", "SIJA", "DKV", "RPL");
+    // dynamic array
+    val sekolah = arrayOf("smk40", "smk46", "smk26", "smk48")
+
+    println("saya kelas ${kelas[1]}")
+    println("saya dari jurusan ${jurusan[0]}")
+    println("saya sekolah di ${sekolah[2]}")
+
+    // in operator untuk mengecek apakah ada
+    if ("PPLG" in jurusan) {
+        println("Ada mapelnya cuyy")
+    } else {
+        println("yah belom ada cuyy")
+    }
+    // array lenght / size
+    println(sekolah.size)
+
+}
