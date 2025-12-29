7a8e02365a9d415bfd95acc1715ac2b7769fa0fd
Kaizen <gibrantsyareza@gmail.com>
2025-12-30 01:54:20 +0700

    -

--- src/basics/variable.kt
+++ src/basics/variable.kt
@@ -1,10 +1,11 @@
-package basics.tipe_data_dasar
+package basics
 
 fun main(){
     // dynamically typing in kotlin
     val name = "jokowi" // immutable atau tidak berubah
     var age = 666 // mutable atau bisa berubah nilainya
 
+    // statically typing
     // typesafe in kotlin
     var namaPertama: String = "kotlin" // sah aja, tapi redundant
     var namaKedua: String = "keren" // kotlin sudah memiliki fitur dynamic typing
